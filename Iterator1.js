22b34a5b87c6e14d778c9adf881731e0538250e7
Jayratna <jayratnashivasharan8@gmail.com>
2026-04-07 09:17:57 +0530

    hey

---
+++ Iterator1.js
@@ -0,0 +1,8 @@
+let str="jay";
+let it=str[Symbol.iterator](); //symbol.iterator is a function for Iterating values
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+
